771fe7298b607c4fcd3d6ed5f38d66e80b41e36c
Hossam Limouni <hlimouni@e1r5p5.1337.ma>
2020-12-10 10:48:27 +0100

    adding the ft_free functions

--- ft_printf/libft/libft.h
+++ ft_printf/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: hlimouni <hlimouni@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/21 18:08:14 by hlimouni          #+#    #+#             */
-/*   Updated: 2020/12/08 13:40:00 by hlimouni         ###   ########.fr       */
+/*   Updated: 2020/12/10 10:43:21 by hlimouni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,8 @@ int					ft_hexlen(int nb);
 int					ft_putccount(char c);
 int					ft_putss(char *str);
 void				ft_lstclear(t_list **lst, void (*del)(void*));
+void    			ft_free_null(void **ptr);
+int					ft_free_ret(void **ptr, int ret);
 t_list				*ft_lstlast(t_list *lst);
 void				ft_lstiter(t_list *lst, void (*f)(void *));
 void				ft_lstdelone(t_list *lst, void (*del)(void*));
@@ -62,6 +64,7 @@ int					ft_memcmp(const void *str1, const void *str2, size_t n);
 void				*ft_memcpy(void *dest, const void *src, size_t n);
 void				*ft_memmove(void *dest, const void *src, size_t len);
 void				*ft_memset(void *dest, int value, size_t len);
+char				*ft_strchr(const char *str, int chr);
 int					ft_strchar_count(char *str, int c);
 int					ft_strncmp(const char *s1, const char *s2, size_t n);
 int					ft_strcmp(const char *s1, const char *s2);
@@ -70,7 +73,6 @@ size_t				ft_strlen(const char *str);
 int					ft_tolower(int c);
 char				*ft_strnstr(const char *s1, const char *s2, size_t n);
 char				*ft_strrchr(const char *str, int c);
-char				*ft_strchr(const char *str, int chr);
 size_t				ft_strnlen(const char *str, size_t maxlen);
 size_t				ft_strlcat(char *dst, const char *src, size_t size);
 
