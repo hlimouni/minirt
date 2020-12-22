d3c1b0bf4bf7a351584d3392462d59a897a0294e
Hossam Limouni <limouni.hossam@gmail.com>
2020-12-22 16:38:58 +0100

    applying make fclean to libft removing objects

--- ft_printf/libft/ft_isspace.c
+++ ft_printf/libft/ft_isspace.c
@@ -6,17 +6,9 @@
 /*   By: hlimouni <hlimouni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/21 14:42:12 by hlimouni          #+#    #+#             */
-/*   Updated: 2020/12/21 18:55:13 by hlimouni         ###   ########.fr       */
+/*   Updated: 2020/12/22 16:25:54 by hlimouni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-/*
-** The ascii values of the backs
-** checks for white-space characters.  In the "C" and "POSIX" locales, these 
-** are: space, form-feed ('\f'), newline ('\n'), carriage return ('\r'),
-** horizontal tab ('\t'), and vertical tab ('\v'). there ascii ranges
-** from 8 to 13.
-*/
 
 #include "libft.h"
 
