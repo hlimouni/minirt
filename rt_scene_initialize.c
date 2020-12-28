75926f948ac7271633f3185a4533601ce6566b13
Hossam Limouni <hlimouni@e1r5p5.1337.ma>
2020-12-28 18:38:48 +0100

    initial commit of rt_scene_initializer

---
+++ rt_scene_initialize.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rt_scene_initialize.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hlimouni <hlimouni@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/28 15:05:18 by hlimouni          #+#    #+#             */
+/*   Updated: 2020/12/28 15:07:21 by hlimouni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minirt.h"
+
+void	rt_scene_initialize(t_scene *scene)
+{
+	scene->res = NULL;
+	scene->amb = NULL;
+	scene->cams = NULL;
+	scene->lights = NULL;
+	scene->objs = NULL;
+}
